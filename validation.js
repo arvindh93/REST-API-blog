d2165334e1f75c53156aef6251282c8efb6c6988
arvindh93 <aravindseshadree93@gmail.com>
2018-09-03 00:47:21 +0530

    Updated validation

--- validation.js
+++ validation.js
@@ -1,8 +1,22 @@
 validation = (req, res, next) => {
   if (req.url == "/posts" || (!req.url.match(/comments/))) {
-    if (req.method == 'post' || req.method == 'put') {
+    if (req.method == 'PUT' || req.method == 'DELETE') {
+      if (typeof req.store[req.params.postId] == "undefined") {
+        res.sendStatus(404)
+      }
+    }
+    if (req.method == 'POST' || req.method == 'PUT') {
       if (!req.name.trim() || !req.url.trim() || !req.text.trim()) {
         res.sendStatus(400)
+      }
+    }
+  } else if (req.url.match(/comments/)) {
+    if (req.method == 'PUT' || req.method == 'DELETE') {
+      if (typeof req.store[req.params.postId] == "undefined") {
+        res.sendStatus(404)
+      }
+      if (typeof req.store[req.params.postId]['comments'][req.params.commentId] == "undefined") {
+        res.sendStatus(404)
       }
     }
   }
